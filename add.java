1f53d915cda202c9dd90383844da2c6763db39b7
gayathric23 <gayathri.chandrasekar23@gmail.com>
2019-09-26 14:48:53 +0530

    Add and Multiply Functions Added

---
+++ add.java
@@ -0,0 +1,8 @@
+public class Add
+{
+	int x,y;
+	public int add(int x, int y)
+	{
+		return (x+y);
+	}
+}
---
+++ multiply.java
@@ -0,0 +1,8 @@
+public class Multiply extends Subtract
+{
+	int x,y;
+	public int multiply(int x, int y)
+	{
+		return (x*y);
+	}
+}
